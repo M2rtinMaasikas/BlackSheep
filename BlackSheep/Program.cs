d413e3ff2446b81b3bb02d532d54dcac5df36f1e
Opilane TTHK <opilane@tthk.ee>
2021-11-16 15:50:26 +0200

    first commit

---
+++ BlackSheep/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Blacksheep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\opilane\samples\black.txt";
+            string[] dataFromFile = File.ReadAllLines(filepath);
+            for (int i = 0; i < dataFromFile.Length; i++)
+            {
+                dataFromFile[i] = dataFromFile[i].Replace('4', 'a');
+                dataFromFile[i] = dataFromFile[i].Replace('0', 'o');
+                dataFromFile[i] = dataFromFile[i].Replace('3', 'e');
+                dataFromFile[i] = dataFromFile[i].Replace('1', 'i');
+
+                foreach (string line in dataFromFile)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
---
+++ ChuckNorrisISJokeReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChuckNorrisISJokeReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
